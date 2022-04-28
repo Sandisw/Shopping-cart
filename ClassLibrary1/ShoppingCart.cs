7fc8b5e89dd4f866b98a78b50ea52ba714467783
Sandiswa <4275420@myuwc.ac.za>
2022-04-28 14:32:44 +0200

    Add project files.

---
+++ ClassLibrary1/ShoppingCart.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShoppingLibrary
+{
+    internal class ShoppingCart
+    {
+        public ShoppingCart=()
+    }
+}
---
+++ Shopping cart/Program.cs
@@ -0,0 +1,17 @@
+﻿using ShoppingLibrary;
+using System;
+
+namespace Shopping_cart
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Class1 test = new Class1();
+            Console.WriteLine("Hello World!");
+
+            
+        }
+    }
+}
