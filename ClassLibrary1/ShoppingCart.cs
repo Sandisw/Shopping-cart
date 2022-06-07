207fa5f1119941af3d1f804809891959895ebd3e
Sandiswa <102956515+Sandisw@users.noreply.github.com>
2022-06-07 13:56:30 +0200

    Update ShoppingCart.cs

--- ClassLibrary1/ShoppingCart.cs
+++ ClassLibrary1/ShoppingCart.cs
@@ -8,6 +8,27 @@ namespace ShoppingLibrary
 {
     internal class ShoppingCart
     {
-        public ShoppingCart=()
+        public ShoppingCart()
+        {
+            InitializeComponent()
+        }
+        
+        int itemsOrdered;
+        const int number Of Products = 3;
+        Product[] products=new Product[numberOfProducts];
+        
+        
+        public void ShoppingCart(object sender, EventArgs)
+        {
+            products[1] = new Product("Noodles", 50);
+            products[2] = new Product("Pasta", 60);
+            products[3] = new Products("Mince" 40);
+            
+            for (int i=0;i< numberOfProducts; i++)
+            {
+               3dudProducts.Item.Add(products[i].Description);
+            }
+            dudProducts.SelectedIndex =0;
+        }  
     }
 }
