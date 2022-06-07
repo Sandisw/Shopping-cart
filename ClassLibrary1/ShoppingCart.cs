a0fc535bd115242108b195a2222356c5ed30e0b9
Sandiswa <102956515+Sandisw@users.noreply.github.com>
2022-06-07 14:43:31 +0200

    Update ShoppingCart.cs

--- ClassLibrary1/ShoppingCart.cs
+++ ClassLibrary1/ShoppingCart.cs
@@ -29,6 +29,70 @@ namespace ShoppingLibrary
                dudProducts.Item.Add(products[i].Description);
             }
             dudProducts.SelectedIndex =0;
-        }  
+        }
+        
+        public void btnExit_click(object sender, EventArgs)
+        {
+            close()
+        }
+        
+        public void by NewOrder_Click()
+        {
+           4txtOrderAddress.Text="";
+            itemsOrdered=0;
+            ItemsOrdered.Text= "Items Ordered:0";
+       
+            for(int i=0; i< numberOfProducts; i++;
+            {
+                products[i].NumberOrdered=0;
+            }
+             
+                dudProducts.SelectedIndex=0;
+                1stProducts.Items?Clear();
+                Total Cost.Text="Total Cost";
+                txtMailingLabel.Text="";
+            }
+             
+            public void AddToOrder_Click()
+            {
+                products[dudProduts.SelectedIndex].NumberOrdered++;
+                itemsOrdered++;
+                ItemsOrdered.Text="ItemsOrdered: "+ itemsOrderd.ToString();
+            }
+                
+            public void ShoppingCart_SelectedIndexChange()
+            {
+                switch(OrderedForm.SelectedIndex)
+                {
+                        if (itemsOrdered ==0)
+                        {
+                            Message Box.Show("Invalid Order",):
+                            order Forms.SelectedIndex=0;
+                        }
+                        else
+                        {
+                            double totalCost=0;
+                            1stProducts.Items.Clear();
+                            for(int i=0; i< numberOfProducts; i++)
+                            {
+                                if(products[i].NumberOrdered i=0)
+                                {
+                                   1stProducts.Items Add(products[i].NumberOrdered.ToString() +""+ products[i].Desciption);
+                                    totalCost+=products [i].Cost* products[i].NumberOrdered;
+                                }
+                            }
+                            1stProducts.Text="Total Cost:150 + String.Format("{0;0}");
+                        }
+                 }
+                        
+                        
+                        
+                    
+                     
+                           
+                              
+             
+   
+         }   
     }
 }
